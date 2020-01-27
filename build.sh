17d53791020df429355dcf69ffb99ba9ae53ceac
Sam Carter <sam.w.carter@gmail.com>
2020-01-27 16:00:16 +0000

    build script update

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 set -x
 
-hugo -b "http://d27j66pmjudaqa.cloudfront.net/"
+hugo -b "http://sam.s-carter.co.uk"
 
 printf ""
-aws s3 cp ./public/ s3://samcarterblog --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
+aws s3 --profile personal cp ./public/ s3://samcarterblog --recursive --grants read=uri=http://acs.amazonaws.com/groups/global/AllUsers
 printf ""
 sleep 3
 printf ""
-aws cloudfront create-invalidation --distribution-id E38IQPAR031UV5 --paths "/*"
+aws cloudfront --profile personal create-invalidation --distribution-id E38IQPAR031UV5 --paths "/*"
